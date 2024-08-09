da9c066c2080dd2c2085a849220f65cc3432a43f
Lok Gubhaju <lok@smal.de>
2024-08-09 12:54:49 +0200

    starter files

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, culpa
+        quaerat corporis nihil iure quisquam, possimus sit saepe facere sint
+        modi maiores quae tempora eius sunt reprehenderit minima aperiam atque,
+        id a tenetur praesentium ipsam dignissimos commodi! Hic cum ipsam
+        expedita modi voluptas, neque rem illum iure necessitatibus, a repellat?
+      </p>
+    </div>
+  );
+}
---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus,
+        molestiae accusamus? Distinctio maxime amet odit doloremque vero unde
+        ut, repellendus eligendi! Sint esse saepe ut.
+      </p>
+    </div>
+  );
+}
