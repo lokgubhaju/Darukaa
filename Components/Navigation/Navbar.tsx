d678c62524148be9af1830210cea7c977ad9b034
Lok Gubhaju <lok@smal.de>
2024-08-11 05:44:35 +0200

    minor changes

--- Components/Navigation/Navbar.tsx
+++ Components/Navigation/Navbar.tsx
@@ -13,57 +13,55 @@ export default function Navbar() {
   const [activeSection, setActiveSection] = useState(null);
 
   const handleScroll = () => {
-    const sections = document.querySelectorAll('section');
-    let currentSection:any = '';
+    const sections = document.querySelectorAll("section");
+    let currentSection: any = "";
 
-    sections.forEach(section => {
+    sections.forEach((section) => {
       const sectionTop = section.offsetTop;
       const sectionHeight = section.clientHeight;
       if (window.scrollY >= sectionTop - sectionHeight / 10) {
-        currentSection = section.getAttribute('id');
+        currentSection = section.getAttribute("id");
       }
     });
-    console.log('Current Section:', currentSection); // Add this line for debugging
+    console.log("Current Section:", currentSection); // Add this line for debugging
     setActiveSection(currentSection);
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     handleScroll();
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-
   return (
     <nav className={cn(s["navigation"])}>
-      <p className={cn(s["navigation__logo"], 'items-center flex text-black')}>Darukaa Earth</p>
-      <div
-        className={cn(s["mobile-navigation"], "block md:hidden")}
-        onClick={handliClick}
-      >
+      <p className={cn(s["navigation__logo"], "items-center flex text-black")}>
+        Darukaa Earth
+      </p>
+      <div className={cn(s["mobile-navigation"])} onClick={handliClick}>
         <span className={s.bar}>Home</span>
         <span className={s.bar}>Introduction</span>
         <span className={s.bar}>Services</span>
       </div>
-      <ul className={cn(s["navigation__lists"], "hidden md:block")}>
-        <li className={activeSection === 'home' ? s.active : ''}>
+      <ul className={cn(s["navigation__lists"])}>
+        <li className={activeSection === "home" ? s.active : ""}>
           <Link href="/">Home</Link>
         </li>
-        <li className={activeSection === 'introduction' ? s.active : ''}>
+        <li className={activeSection === "introduction" ? s.active : ""}>
           <Link href="#introduction">Introduction</Link>
         </li>
-        <li className={activeSection === 'what-we-do' ? s.active : ''}>
+        <li className={activeSection === "what-we-do" ? s.active : ""}>
           <Link href="#what-we-do">What we do</Link>
         </li>
-        <li className={activeSection === 'services' ? s.active : ''}>
+        <li className={activeSection === "services" ? s.active : ""}>
           <Link href="#services">Services</Link>
         </li>
-        <li className={activeSection === 'solutions' ? s.active : ''}>
+        <li className={activeSection === "solutions" ? s.active : ""}>
           <Link href="#solutions">Solutions</Link>
         </li>
-        <li className={activeSection === 'contact' ? s.active : ''}>
+        <li className={activeSection === "contact" ? s.active : ""}>
           <Link href="#contact">Contact us</Link>
         </li>
       </ul>
