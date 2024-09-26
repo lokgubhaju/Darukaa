8e655c8edfbc6a1d75525da62873a64d67fc6f22
Lok Gubhaju <lok@smal.de>
2024-09-26 02:03:22 +0200

    added gtm, favicon, metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import {gsap} from 'gsap'
+import Script from "next/script";
 
 gsap.registerPlugin(ScrollTrigger);
 const poppins = Poppins({
@@ -15,6 +16,19 @@ const poppins = Poppins({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={poppins.className}>
+      <Script
+        id="gtm-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','G-TMERQ3HFH6');
+          `,
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,51 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        {/* HTML Meta Tags */}
+        <meta name="description" content="" />
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://www.darukaa.earth" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Darukaa Earth | Home" />
+        <meta property="og:description" content="" />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/cf1a3e4f-a126-49b0-a4c2-0684e4fbd4ed.png?token=ahb95d2qm2Ha1YVqnFlcJU6FFZ3ziPeAYLJR1ibx0fE&height=480&width=852&expires=33263307438"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="darukaa.earth" />
+        <meta property="twitter:url" content="https://www.darukaa.earth" />
+        <meta name="twitter:title" content="Darukaa Earth | Home" />
+        <meta name="twitter:description" content="" />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/cf1a3e4f-a126-49b0-a4c2-0684e4fbd4ed.png?token=ahb95d2qm2Ha1YVqnFlcJU6FFZ3ziPeAYLJR1ibx0fE&height=480&width=852&expires=33263307438"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
